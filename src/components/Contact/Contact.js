9b193446b1601fe1d60d05e42e9d3e62b35fb909
Ikeo <ikeomajesty@gmail.com>
2019-07-11 11:58:21 +0200

    contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,9 +4,26 @@ import styles from '../../css/contact.module.css'
 
 const Contact = () => {
     return (
-        <div>
-            Hello from Contact
-        </div>
+        <section className={styles.contact}>
+            <Title title="get in" subtitle="touch" />
+            <div className={styles.center}>
+                <form className={styles.form} action="https://formspree.io/mrikeorl@gmail.com" method="POST">
+                <div>
+                    <input type="text" name="name" id="name" className={styles.formControl} placeholder="Name"/>
+                </div>
+                <div>
+                    <input type="email" name="email" id="email" className={styles.formControl} placeholder="Email"/>
+                </div>
+                <div>
+                    <textarea name="message" id="message" rows="10" className={styles.formControl} placeholder="Your message" />
+
+                </div>
+                <div>
+                    <input type="submit" value="Submit" className={styles.submit}/>
+                </div>
+                </form>
+            </div>
+        </section>
     )
 }
 
